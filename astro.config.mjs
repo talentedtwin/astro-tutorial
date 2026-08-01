290ecb7ab0b297bde3f1b31db18b0d2db8e7f3ea
talentedtwin <peter.n.fleming@gmail.com>
2026-08-01 21:18:12 +0100

    meta description and canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-tutorial-gamma-eight.vercel.app/",
+  site: "https://astro-tutorial-gamma-eight.vercel.app",
   integrations: [preact()],
 });
